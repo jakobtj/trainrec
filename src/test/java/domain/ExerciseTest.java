3282d3c841b2b95862402aa54d0868d4686705f6
Jakob <jjt@fastmail.com>
2020-01-06 19:02:29 +0100

    Rename constructor test

--- src/test/java/domain/ExerciseTest.java
+++ src/test/java/domain/ExerciseTest.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class ExerciseTest {
 
     @Test
-    public void testExerciseInit() {
+    public void testExerciseConstructor() {
         Date today = new Date();
         Exercise ex = new Exercise("Squat", today);
     }
