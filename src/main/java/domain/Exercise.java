75d997ab697a41431c9f25f28e644e97df76df2b
Jakob <jjt@fastmail.com>
2020-01-06 18:38:22 +0100

    Add basic project structure

---
+++ src/main/java/domain/Exercise.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Exercise {
+    private String name;
+
+    public Exercise(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/domain/ExerciseTest.java
@@ -0,0 +1,12 @@
+package domain;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExerciseTest {
+
+    @Test
+    public void testExerciseInit() {
+        Exercise ex = new Exercise("Squats");
+    }
+}
