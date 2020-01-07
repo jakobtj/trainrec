cacf224050f255e43ba80683470073f5fa3948d2
Jakob <jjt@fastmail.com>
2020-01-07 02:41:59 +0100

    Fix syntax errors

--- src/test/java/domain/UseCaseTest.java
+++ src/test/java/domain/UseCaseTest.java
@@ -6,16 +6,20 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Date;
 
+import domain.ExerciseEntry;
+import domain.Exercise;
+import usecases.ExerciseEntryAdd;
+
 public class UseCaseTest {
     @Test
     public void testExerciseEntryAdd() {
-        ArrayList<ExerciseEntry> entries = new ArrayList();
+        ArrayList<ExerciseEntry> entries = new ArrayList<ExerciseEntry>();
         Date today = new Date();
         Exercise squat = new Exercise("Squat");
-        ExerciseEntry squatEntry = new ExerciseEntry(squat, today);
+        ExerciseEntry squatEntry = new ExerciseEntry(today, squat);
 
         ExerciseEntryAdd adder = new ExerciseEntryAdd(entries);
-        ExerciseEntryAdd.execute(squatEntry);
+        adder.execute(squatEntry);
 
         assertTrue(entries.contains(squatEntry));
     }
