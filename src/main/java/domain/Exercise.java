ed3529ec598f11b6bb7830228a0fd67cc169ff39
Jakob <jjt@fastmail.com>
2020-01-07 02:41:23 +0100

    Remove date parameter

--- src/main/java/domain/Exercise.java
+++ src/main/java/domain/Exercise.java
@@ -1,21 +1,9 @@
 package domain;
 
-import java.util.Date;
-
 public class Exercise {
     private String name;
-    private Date date;
 
-    public Exercise(String name, Date date) {
+    public Exercise(String name) {
         this.name = name;
-        this.date = date;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Date getDate() {
-        return date;
     }
 }
