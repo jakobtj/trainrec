4fdc5d52ada0ecc581ebaac806b6f5e0ca06f5dd
Jakob <jjt@fastmail.com>
2020-01-10 00:47:36 +0100

    Compare exercise entries

--- src/main/java/ExerciseEntry.java
+++ src/main/java/ExerciseEntry.java
@@ -2,12 +2,18 @@ package trainrec;
 
 import java.time.LocalDate;
 
-public class ExerciseEntry {
+class ExerciseEntry {
     private LocalDate date;
     private Exercise exercise;
 
     public ExerciseEntry(LocalDate date, Exercise exercise) {
         this.date = date;
         this.exercise = exercise;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        ExerciseEntry other = (ExerciseEntry) object;
+        return date.equals(other.date) && exercise.equals(other.exercise);
     }
 }
--- src/test/java/TrainRecTest.java
+++ src/test/java/TrainRecTest.java
@@ -17,6 +17,26 @@ public class TrainRecTest {
     }
 
     @Test
+    public void testCompareExerciseEntries() {
+        ExerciseEntry first = new ExerciseEntry(
+                LocalDate.of(2020, 1, 1), new Exercise("Squat")
+                );
+        ExerciseEntry second = new ExerciseEntry(
+                LocalDate.of(2020, 1, 1), new Exercise("Squat")
+                );
+        ExerciseEntry third = new ExerciseEntry(
+                LocalDate.of(2021, 12, 31), new Exercise("Squat")
+                );
+        ExerciseEntry fourth = new ExerciseEntry(
+                LocalDate.of(2020, 1, 1), new Exercise("Bench press")
+                );
+
+        Assert.assertEquals(second, first);
+        Assert.assertNotEquals(third, first);
+        Assert.assertNotEquals(fourth, first);
+    }
+
+    @Test
     public void testCreateEmptyTrainingRecord() {
         TrainingRecord rec = new TrainingRecord();
         Assert.assertEquals(0, rec.getExerciseCount());
