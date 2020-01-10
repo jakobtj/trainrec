a0dfa8ab3c33517c8d9e85e38ac01f1dea0dbf44
Jakob <jjt@fastmail.com>
2020-01-10 01:21:48 +0100

    Test string split method

--- src/test/java/ExternalsTest.java
+++ src/test/java/ExternalsTest.java
@@ -1,4 +1,4 @@
-package LocalDateTest;
+package ExternalsTest;
 
 import org.junit.Test;
 import org.junit.Assert;
@@ -6,11 +6,19 @@ import org.junit.Assert;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class LocalDateTest {
+public class ExternalsTest {
     @Test
     public void testParseDate() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
         LocalDate parsed = LocalDate.parse("2020.01.10", formatter);
         Assert.assertEquals(LocalDate.of(2020, 1, 10), parsed);
+   }
+
+   @Test
+   public void testSplitString() {
+       String inString = "2020.01.10;Squat";
+       String[] splitString = inString.split(";");
+       Assert.assertEquals("2020.01.10", splitString[0]);
+       Assert.assertEquals("Squat", splitString[1]);
    }
 }
